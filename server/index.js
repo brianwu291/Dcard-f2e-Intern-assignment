748e722c8477a312df3701664be2b939e15ed388
BrianWu <brianwu291@gmail.com>
2019-01-28 17:16:30 +0800

    Revert "add port adjust in server file"
    This reverts commit 81a52e3434bed138942c16db41d8e4b6a1b841fa.

--- server/index.js
+++ server/index.js
@@ -7,8 +7,7 @@ const router = new Router();
 router.use(require('koa-body')());
 router.use('/api/todos', require('./todos'));
 
-const PORT = 3001 || process.env.PORT;
 app
   .use(router.routes())
   .use(router.allowedMethods())
-  .listen(PORT);
+  .listen(3001);
