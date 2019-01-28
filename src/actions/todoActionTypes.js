841da0703bea9c4bc68537574ff0e654ec824f4e
BrianWu <brianwu291@gmail.com>
2019-01-28 17:46:39 +0800

    action typo adjust

--- src/actions/todoActionTypes.js
+++ src/actions/todoActionTypes.js
@@ -1,4 +1,4 @@
-export const INIT_TODO = 'INIT_TODOM'; // GET: get all todo lists
+export const INIT_TODO = 'INIT_TODO'; // GET: get all todo lists
 export const ADD_TODO = 'ADD_TODO';  // POST: add new todo list
 export const EDIT_TODO = 'EDIT_TODO';  // PUT: update todo list
 export const DELETE_TODO = 'DELETE_TODO';  //DELETE: delete todo list
